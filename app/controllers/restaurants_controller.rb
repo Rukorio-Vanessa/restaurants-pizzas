d3b0341c4435f37f4de15b6b940757cdd261166b
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-20 14:16:10 +0300

    created show method for GET request

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,14 @@ class RestaurantsController < ApplicationController
     def index
         render json: Restaurant.all
     end
+
+    #GET: display  restaurant by id
+    def show
+        restaurant = Restaurant.find_by(id: params[:id])
+        if restaurant
+            render json: restaurant
+        else
+            render json: {error: "Restaurant not found"}, status: :not_found
+        end
+    end
 end
