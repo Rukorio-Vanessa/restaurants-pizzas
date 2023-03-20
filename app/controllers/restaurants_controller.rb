10495727036d7b814dd005b6981f445e3522177d
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-20 16:55:39 +0300

    used rescue_from mthd to handle errors

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class RestaurantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
     #GET: display all restaurants 
     def index
         render json: Restaurant.all
@@ -6,30 +8,24 @@ class RestaurantsController < ApplicationController
 
     #GET: display  restaurant by id
     def show
-        restaurant = Bird.find(params[:id])
-
-        if restaurant
-            render json: restaurant, include: :pizzas
-        else
-            render json: {error: "Restaurant not found"}, status: :not_found
-        end
+        restaurant = find_restaurant
+        render json: restaurant, include: :pizzas
     end
 
     #DELETE: delete resaturany by id
     def destroy
         restaurant = find_restaurant
-        if restaurant
-            restaurant.destroy
-            render json: {}, status: :no_content
-        else
-            render json: {error: "Restaurant not found"}, status: :not_found
-   
-        end     
+        restaurant.destroy
+        render json: {}, status: :no_content
+       
     end
 
     private
     def find_restaurant
         Restaurant.find(params[:id])
     end
-
+   
+    def render_not_found_response
+        render json: { error: "Restaurant not found" }, status: :not_found
+    end
 end
