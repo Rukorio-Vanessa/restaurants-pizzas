ea3dc191a2db4b4b7b7bcea43fbd3edd2f3e8f2d
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-20 11:42:30 +0300

    added foreign keys to reataurant_pizzas

---
+++ db/migrate/20230320042607_add_pizza_and_restaurant_to_restaurant_pizza.rb
@@ -0,0 +1,6 @@
+class AddPizzaAndRestaurantToRestaurantPizza < ActiveRecord::Migration[7.0]
+  def change
+    add_column :restaurant_pizzas, :pizza_id, :integer
+    add_column :restaurant_pizzas, :restaurant_id, :integer
+  end
+end
