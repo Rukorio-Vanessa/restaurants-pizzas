2b0891716f58d06cd5ccacc6e770b0b5a47c9488
Rukorio-Vanessa <blwwbl55@gmail.com>
2023-03-20 15:00:24 +0300

    defined  create mthd to make restaurantpizzas post request

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,2 +1,7 @@
 class RestaurantPizzasController < ApplicationController
+    #POST: create RestaurantPizza associated with an existing Pizza and Restaurant
+    def create
+        restaurant_pizza = RestaurantPizza.create(price: params[:price], pizza_id: params[:pizza_id], restaurant_id: params[:restaurant_id] ) 
+        render json: restaurant_pizza, status: :created
+    end
 end
